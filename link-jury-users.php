53d29a9db23bf825e0ebdb992a9a5c96d08f80c7
Nicolas Estrem <103498114+nicolasestrem@users.noreply.github.com>
2025-06-16 14:38:47 +0200

    Delete link-jury-users.php

--- link-jury-users.php
+++
@@ -1,8 +0,0 @@
-<?php
-$orphan_users = get_users(array('role' => 'mt_jury_member'));
-$create_missing = true; // Set to true to create jury posts for users without matches
-
-if ($create_missing && count($unlinked_users) > 0) {
-    echo "Creating jury member posts for unmatched users...\n";
-// ... existing code ...
-} 
